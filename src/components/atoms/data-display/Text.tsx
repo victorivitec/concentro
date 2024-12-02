b18961d2de72de0d8a5f938661ba1de4cf40c4d6
victorivitec <victor.carrillo@ivitec.com.mx>
2024-12-02 17:55:15 -0600

    fix text

--- src/components/atoms/data-display/Text.tsx
+++ src/components/atoms/data-display/Text.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Typography, useTheme } from "@mui/material";
 import classNames from "classnames";
 import s from "./Text.module.scss";
 
@@ -35,21 +35,53 @@ interface TextProps {
     | "overline"; //variant
 }
 
-const variantStyles: Record<string, React.CSSProperties> = {
-  h1: { fontSize: "6.8em" },
-  h2: { fontSize: "6em" },
-  h3: { fontSize: "4.9em" },
-  h4: { fontSize: "3.8em" },
-  h5: { fontSize: "2.7em" },
-  h6: { fontSize: "1.8em" },
-  subtitle1: { fontSize: "1.3em", fontWeight: "bold" },
-  subtitle2: { fontSize: "0.85em", fontWeight: "bold" },
-  body1: { fontSize: "1em" },
-  body2: { fontSize: "0.92em" },
+const variantStyles = (theme: any): Record<string, React.CSSProperties> => ({
+  h1: {
+    fontSize: "6.8em",
+    [theme.breakpoints.down("sm")]: { fontSize: "2.5em" },
+  },
+  h2: {
+    fontSize: "6em",
+    [theme.breakpoints.down("sm")]: { fontSize: "2.3em" },
+  },
+  h3: {
+    fontSize: "4.9em",
+    [theme.breakpoints.down("sm")]: { fontSize: "2em" },
+  },
+  h4: {
+    fontSize: "3.8em",
+    [theme.breakpoints.down("sm")]: { fontSize: "1.8em" },
+  },
+  h5: {
+    fontSize: "2.7em",
+    [theme.breakpoints.down("sm")]: { fontSize: "1.5em" },
+  },
+  h6: {
+    fontSize: "1.8em",
+    [theme.breakpoints.down("sm")]: { fontSize: "1.3em" },
+  },
+  subtitle1: {
+    fontSize: "1.3em",
+    fontWeight: "bold",
+    [theme.breakpoints.down("sm")]: { fontSize: "1em" },
+  },
+  subtitle2: {
+    fontSize: "0.85em",
+    fontWeight: "bold",
+    [theme.breakpoints.down("sm")]: { fontSize: "0.75em" },
+  },
+  body1: {
+    fontSize: "1em",
+    [theme.breakpoints.down("sm")]: { fontSize: "0.9em" },
+  },
+  body2: {
+    fontSize: "0.92em",
+    [theme.breakpoints.down("sm")]: { fontSize: "0.8em" },
+  },
   caption: {},
   button: {},
   overline: {},
-};
+});
 
 export const Text: React.FC<TextProps> = (props) => {
   const {
@@ -62,6 +94,7 @@ export const Text: React.FC<TextProps> = (props) => {
     className,
   } = props;
 
+  const theme = useTheme(); // Acceder al tema de Material-UI
   const textClasses = classNames(s.text, s[`text__${type}`], className);
 
   return (
@@ -70,7 +103,7 @@ export const Text: React.FC<TextProps> = (props) => {
       variant={type}
       color={color}
       component={component}
-      sx={{ ...variantStyles[type], ...sx }}
+      sx={{ ...variantStyles(theme)[type], ...sx }}
       className={textClasses}
     >
       {children}
