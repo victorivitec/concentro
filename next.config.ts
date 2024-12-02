d972c825943ecd303d8d9ab6f4ec6e9b72ff4577
victorivitec <victor.carrillo@ivitec.com.mx>
2024-12-02 16:27:37 -0600

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  reactStrictMode: true, // Opcional, mejora el desarrollo con advertencias adicionales.
+  swcMinify: true, // Usa el compilador SWC para minimizar el código.
 };
 
 export default nextConfig;
